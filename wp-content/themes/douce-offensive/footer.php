b85c340c126c2dc16bf8cee593309332a7f6f662
Alexandre Girard <alx.girard@gmail.com>
2008-11-19 02:23:14 +0100

    Revert "use 3 column layout"
    This reverts commit 17a1c2523805bfe90a34bb4918e6553eadc6d517.

--- wp-content/themes/douce-offensive/footer.php
+++
@@ -1,8 +0,0 @@
-<font class="credit">
-Powered by <a href='http://wordpress.org'><strong>WordPress</strong></a>
-</p>
-
-
-</body>
-
-</html>
--- wp-content/themes/douce-offensive/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-if ( function_exists('register_sidebars') )
-    register_sidebar('1'); register_sidebar('2')
-?>
