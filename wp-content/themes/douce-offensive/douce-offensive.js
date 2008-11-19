9e558b4aa803613085a31d394bb24b898217d7bd
Alexandre Girard <alx.girard@gmail.com>
2008-11-19 23:50:40 +0100

    add brder on new photo

--- wp-content/themes/douce-offensive/douce-offensive.js
+++ wp-content/themes/douce-offensive/douce-offensive.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function() {
-	jQuery(".nav_photo").click(function() { jQuery(".photoQcontent").attr("src", jQuery(this).attr("url"));});
+	jQuery(".nav_photo").click(function() {
+		jQuery(this).attr("url");
+		jQuery(".photoQcontent").attr("src", jQuery(this).attr("url"));
+		jQuery("div.nav_photo").css("border","#000 1px solid");
+	    jQuery(this).css("border","#fff 1px solid");
+	});
 });
