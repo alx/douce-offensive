0c9fff3b92f08ba191a920dd82e639d18b5830bf
Alexandre Girard <alx.girard@gmail.com>
2008-11-19 20:02:43 +0100

    move js in script

--- wp-content/themes/douce-offensive/footer.php
+++ wp-content/themes/douce-offensive/footer.php
@@ -6,6 +6,14 @@
 
 </div><!-- page -->
 
+<script type="text/javascript" charset="utf-8">
+	$(document).ready(function() {
+		$("div.nav_photo").click(function() {
+			$("#main_photo").attr("src",$(this).attr("url"));
+		});
+	});
+</script>
+
 <?php do_action('wp_footer'); ?>
 
 </body>
