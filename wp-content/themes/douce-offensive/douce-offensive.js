7b0e65847c285cc61d4b9633ad89a2c80ddb8b94
Alexandre Girard <alx.girard@gmail.com>
2008-11-20 22:35:17 +0100

    Try solution from stackoverflow

--- wp-content/themes/douce-offensive/douce-offensive.js
+++ wp-content/themes/douce-offensive/douce-offensive.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 	jQuery(".nav_photo").click(function() {
 		jQuery(this).attr("url");
 		jQuery(".photoQcontent").attr("src", jQuery(this).attr("url"));
-		jQuery("#photo_title").html(jQuery(this).children("img").attr("alt"));
+		jQuery("#photo_title").html(jQuery("img", this).attr("alt"));
 		jQuery(".nav_photo").css("border","#000 1px solid");
 	    jQuery(this).css("border","#fff 1px solid");
 	});
