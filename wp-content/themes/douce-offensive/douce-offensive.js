4a0849771fde6dd9cfe07f076e091ed657f8c88a
Alexandre Girard <alx.girard@gmail.com>
2008-11-20 20:30:45 +0100

    modify title whne click

--- wp-content/themes/douce-offensive/douce-offensive.js
+++ wp-content/themes/douce-offensive/douce-offensive.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
 	jQuery(".nav_photo").click(function() {
 		jQuery(this).attr("url");
 		jQuery(".photoQcontent").attr("src", jQuery(this).attr("url"));
+		jQuery("#photo_title").html(jQuery(this).jQuery(img).attr("alt"));
 		jQuery("div.nav_photo").css("border","#000 1px solid");
 	    jQuery(this).css("border","#fff 1px solid");
 	});
