0b0acc1ea9b6c9b4d19076fccb90a6f7f5fc0570
Alexandre Girard <alx.girard@gmail.com>
2009-09-15 09:53:42 +0200

    add horizontal arrwo

--- wp-content/themes/rosa/category.php
+++ wp-content/themes/rosa/category.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
+<a class="prevPage browse left disabled"/>
+
 <div id="wheeled" class="scrollable">
 	<div class="items">
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -11,6 +13,9 @@
 <?php endwhile; endif; ?>
 	</div>
 </div>
+
+<a class="nextPage browse right"/>
+<br clear="all"/>
 
 <script>
 $(document).ready(function() {
