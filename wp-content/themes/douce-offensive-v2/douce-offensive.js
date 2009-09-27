6929860d40d0c041d8d93650055eb766a24abd2a
Alexandre Girard <alx.girard@gmail.com>
2009-09-27 18:11:51 +0200

    update dov2 style

--- wp-content/themes/douce-offensive-v2/douce-offensive.js
+++ wp-content/themes/douce-offensive-v2/douce-offensive.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function() {
 	
-	var all_img_width = 150;
+	var all_img_width = 0;
 	
 	jQuery("#photobook img").each(function() {
-		all_img_width += (this.width + 30);
+		all_img_width += (this.width + 25);
 	})
 	
 	jQuery("#content").width(all_img_width);
