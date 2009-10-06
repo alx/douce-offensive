9e82abad095fae5a717af8cdc869d24da19ef716
Alexandre Girard <alx.girard@gmail.com>
2009-10-06 18:50:47 +0200

    remove auto width

--- wp-content/themes/douce-offensive-v2/douce-offensive.js
+++ wp-content/themes/douce-offensive-v2/douce-offensive.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
 	
-	var all_img_width = 300;
+	var all_img_width = 460;
 	
 	jQuery("#photobook .photo").each(function() {
 		all_img_width += (this.width + 50);
