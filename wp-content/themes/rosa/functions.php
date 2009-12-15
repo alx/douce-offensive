52e28c5d969a93b04abca4385f48a9ff0231fd6d
Alexandre Girard <alx.girard@gmail.com>
2009-12-15 14:56:19 +0100

    change display_recent_categories

--- wp-content/themes/rosa/functions.php
+++ wp-content/themes/rosa/functions.php
@@ -49,16 +49,24 @@ if ( function_exists('register_sidebar') )
 	));
 
 function display_recent_categories(){
-	global $wpdb, $wp_rewrite;
 	
-	$query = "select t.term_id as term_ids, t.name, t.slug, max(p.ID) as id, tx.count from $wpdb->terms t, $wpdb->term_taxonomy tx, $wpdb->term_relationships tr, $wpdb->posts p where t.term_id = tx.term_id and tx.parent != 0 and tx.taxonomy = 'category' and tr.term_taxonomy_id = t.term_id and tr.object_id = id and p.post_status = 'publish' group by term_ids order by id desc limit 5";
+	// $query = "select t.term_id as term_ids, t.name, t.slug, max(p.ID) as id, tx.count from $wpdb->terms t, $wpdb->term_taxonomy tx, $wpdb->term_relationships tr, $wpdb->posts p where t.term_id = tx.term_id and tx.parent != 0 and tx.taxonomy = 'category' and tr.term_taxonomy_id = t.term_id and tr.object_id = id and p.post_status = 'publish' group by term_ids order by id desc limit 5";
+	// 
+	// $results = $wpdb->get_results($query);
+	// 
+	// foreach ($results as $result) {
+	// 	$catlink = str_replace( '%category%', $result->slug, $wp_rewrite->get_category_permastruct());
+	// 	$catlink = get_option( 'home' ) . user_trailingslashit( $catlink, 'category' );
+	// 	echo "<li><a href='".$catlink."'>".$result->name."</a></li>";
+	// }
 	
-	$results = $wpdb->get_results($query);
-	
-	foreach ($results as $result) {
-		$catlink = str_replace( '%category%', $result->slug, $wp_rewrite->get_category_permastruct());
-		$catlink = get_option( 'home' ) . user_trailingslashit( $catlink, 'category' );
-		echo "<li><a href='".$catlink."'>".$result->name."</a></li>";
+	$args=array(
+	  'orderby' => 'name',
+	  'order' => 'ASC'
+	  );
+	$categories=get_categories($args);
+	foreach($categories as $category) { 
+		echo '<li><a href='" . get_category_link( $category->term_id ) . "'>' . $category->name . '</a></li>';
 	}
 }
 
